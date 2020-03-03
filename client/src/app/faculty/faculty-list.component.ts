0d1d3753947218e240f0f658b4fe1666072ddb1e
BananaObserved <golds349@morris.umn.edu>
2020-03-03 15:54:06 -0600

    Add selector to faculty-list.component

--- client/src/app/faculty/faculty-list.component.ts
+++ client/src/app/faculty/faculty-list.component.ts
@@ -1,4 +1,11 @@
-import { OnInit } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-list-component',
+  templateUrl: 'faculty-list.component.html',
+  styleUrls: [],
+  providers: []
+})
 
 export class FacultyListComponent implements OnInit {
   ngOnInit(): void {
