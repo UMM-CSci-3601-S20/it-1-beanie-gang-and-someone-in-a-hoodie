3319a5ec1c1ab94e3815cd1f11aec5ea29ccfe6c
Kyle Fluto <scv.stack@gmail.com>
2020-03-05 18:29:15 -0600

    Create owner page html and supporting typescript
    Create the HTML page for an owner, and the connected typescript necessary.  Also create a preliminary version of node.service, in order to allow the owner page component to subscribe.  The node.service currently only makes vacuous api calls.

--- client/src/app/notes/note.service.ts
+++ client/src/app/notes/note.service.ts
@@ -1,3 +1,33 @@
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+import { Note } from './note';
+
+@Injectable()
 export class NoteService {
-  constructor() {}
+  readonly noteUrl: string = environment.API_URL + 'notes';
+
+  constructor(private httpClient: HttpClient) {}
+
+
+  /**
+   *
+   * @param ownerId: identifier number of the owner
+   * whose notes are being retrieved.
+   *
+   * @returns a map of the notes belonging to this owner,
+   * with the keys 'active', 'template', 'draft', and 'deleted'.
+   *
+   */
+
+  getNotesByOwner(ownerId: string): Observable<Map<string, Note[]>> {
+    let httpParams: HttpParams = new HttpParams();
+    httpParams = httpParams.set('id', ownerId);
+    return this.httpClient.get<Map<string, Note[]>>(this.noteUrl, {
+      params: httpParams,
+    });
+  }
+
 }
--- client/src/app/owner/owner-page.component.ts
+++ client/src/app/owner/owner-page.component.ts
@@ -1,17 +1,60 @@
-import { OnInit, Component } from '@angular/core';
+import { OnInit, Component, OnDestroy } from '@angular/core';
+import { Note } from '../notes/note';
+import { OwnerService } from './owner.service';
+import { Owner } from './owner';
+import { Subscription } from 'rxjs';
+import { NoteService } from '../notes/note.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-owner-page-component',
   templateUrl: 'owner-page.component.html',
-  styleUrls: ['./owner-page.component.scss'],
+  styleUrls: ['owner-page.component.scss'],
   providers: []
 })
 
-export class OwnerPageComponent implements OnInit {
+export class OwnerPageComponent implements OnInit, OnDestroy {
+
+  constructor(private ownerService: OwnerService,
+              private noteService: NoteService, private route: ActivatedRoute) { }
+
+  // Keys: 'active', 'template', 'deleted', 'draft'
+  public notes: Map<string, Note[]>;
+
+  owner: Owner;
+  id: string;
+  getOwnerSub: Subscription;
+  getNotesSub: Subscription;
+
+
+
+
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    // Subscribe to the correct owner and their notes
+    this.route.paramMap.subscribe((pmap) => {
+      this.id = pmap.get('id');
+      if (this.getOwnerSub) {
+        this.getOwnerSub.unsubscribe();
+      }
+      this.getOwnerSub = this.ownerService.getOwnerById(this.id).subscribe(owner => this.owner = owner);
+
+      if (this.getNotesSub) {
+        this.getNotesSub.unsubscribe();
+      }
+      this.getNotesSub = this.noteService.getNotesByOwner(this.id).subscribe(notes => this.notes = notes);
+    });
   }
 
-  constructor() {}
+
+
+  ngOnDestroy(): void {
+    if (this.getOwnerSub) {
+      this.getOwnerSub.unsubscribe();
+    }
+
+    if (this.getNotesSub) {
+      this.getNotesSub.unsubscribe();
+    }
+  }
 
 }
