5175504f0ec468095219c76d77c0b305bdd89c18
Kyle Fluto <scv.stack@gmail.com>
2020-03-05 19:24:06 -0600

    Fix merge conflict

--- client/src/testing/note.service.mock.ts
+++ client/src/testing/note.service.mock.ts
@@ -3,11 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Note, NoteStatus } from '../app/notes/note';
 import { NoteService } from '../app/notes/note.service';
 
-<<<<<<< HEAD
 @Injectable()
-=======
-@Injectable
->>>>>>> owner-page
 export class MockNoteService extends NoteService {
   static testActiveNotes: Note[] = [
     {
