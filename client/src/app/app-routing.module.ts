7606f9c7385180cdd6543dcf7265f70228fe7a76
scvstackkk <scv.stack@gmail.com>
2020-03-11 12:22:39 -0500

    Remove user components & files

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { UserListComponent } from './users/user-list.component';
-import { UserProfileComponent } from './users/user-profile.component';
-import { AddUserComponent } from './users/add-user.component';
 import { OwnerListComponent } from './owner/owner-list.component';
 import { AddOwnerComponent } from './owner/add-owner.component';
 import { OwnerPageComponent } from './owner/owner-page.component';
@@ -14,14 +11,11 @@ import { AddNoteComponent } from './notes/add-note.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'users', component: UserListComponent},
-  {path: 'users/new', component: AddUserComponent},
-  {path: 'users/:id', component: UserProfileComponent},
   {path: 'owners', component: OwnerListComponent},
   {path: 'owners/new', component: AddOwnerComponent},
   {path: 'owners/:id', component: OwnerPageComponent},
-  //{path: 'notes', component: OwnerPageComponent},
-  //{path: 'notes/new', component: EditNoteComponent},
+  {path: 'notes', component: OwnerPageComponent},
+  {path: 'notes/new', component: AddNoteComponent},
   {path: 'notes/edit', component: EditNoteComponent}
 ];
 
